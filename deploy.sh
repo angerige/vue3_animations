39523b972c3941f5eeb1661ae7575cd089806191
Angerige <1652572107@qq.com>
2021-03-08 11:16:15 +0800

    静态部署

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,6 @@ git add -A
 git commit -m 'deploy'
 
 # 提交到 https://github.com:nusr/resume-vue 项目的 gh-pages 分支
-git push -f git@github.com:angerige/vue3_animations.git main:gh-pages
+git push -f git@github.com:angerige/vue3_animations.git master:gh-pages
 
 cd -
